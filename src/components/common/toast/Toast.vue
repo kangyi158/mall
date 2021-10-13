d248981d69b0bd888cd1974cd8575157b0dc24f2
kangyi158 <2712667546@qq.com>
2021-10-14 00:10:05 +0800

    toasst

---
+++ src/components/common/toast/Toast.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="toast" v-show="isShow">
+    <div>{{ message }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Toast",
+  props: {
+    message: {
+      type: String,
+      default: "",
+    },
+    isShow: {
+      type: Boolean,
+      dafault: false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.toast {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 8px 10px;
+  color: #fff;
+  background-color: rgba(0, 0, 0, 0.7);
+}
+</style>>
+</style>
